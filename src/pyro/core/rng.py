8bcd0043ef20a3ce34ad8a728e72dab74397bad4
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2024-03-23 16:35:44 -0700

    [brief] Refactor of the RNG system.
    [detailed]
    - The new linter settings prompted a re-evaluation of the RNG system
      from Numpy, and it seems we're using a legacy API. Numpy recommends
      the usage of generators instead of the old API. Since Generators have
      to be manually created, it should be the user's responsibility to save
      the state and restore it (if required).

--- src/pyro/core/rng.py
+++ src/pyro/core/rng.py
@@ -1,8 +1,6 @@
 import dataclasses
 import random
-import typing
 
-import numpy as np
 import torch
 
 
@@ -14,12 +12,10 @@ class RNGState:
     Args:
         torch_state (torch.Tensor): random state for torch.
         cuda_state (torch.Tensor): random state for torch.cuda.
-        numpy_state (Dict[str, Any]): random state for numpy.
         rand_state (Tuple): random state for Python's random.
     """
 
     torch_state: torch.Tensor
-    numpy_state: dict[str, typing.Any]
     rand_state: tuple
     cuda_state: torch.Tensor | None = None
 
@@ -30,7 +26,7 @@ def seed_rngs(seed: int) -> None:
     """
     Seed the default RNGs with the given seed.
 
-    Default RNGs are: PyTorch (+ CUDA if available), Numpy, and Random.
+    Default RNGs are: PyTorch (+ CUDA if available) and Random.
 
     Args:
         seed (int): value to seed the random generators with.
@@ -39,14 +35,13 @@ def seed_rngs(seed: int) -> None:
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
     random.seed(seed)
-    np.random.seed(seed)
 
 
 def get_rng_state() -> RNGState:
     """
     Get the state for the default RNGs.
 
-    Default RNGs are: PyTorch (+ CUDA if available), Numpy, and Random.
+    Default RNGs are: PyTorch (+ CUDA if available) and Random.
 
     Returns:
         RandomState: the state of all RNGs.
@@ -54,7 +49,6 @@ def get_rng_state() -> RNGState:
     return RNGState(
         torch_state=torch.get_rng_state(),
         cuda_state=torch.cuda.get_rng_state() if torch.cuda.is_available() else None,
-        numpy_state=np.random.get_state(),
         rand_state=random.getstate(),
     )
 
@@ -72,5 +66,4 @@ def restore_rng_state(state: RNGState) -> None:
     if torch.cuda.is_available() and state.cuda_state is not None:
         torch.cuda.set_rng_state(state.cuda_state)
 
-    np.random.set_state(state.numpy_state)
     random.setstate(state.rand_state)
