347221678d2fb55e93276f072fa13aa86a18ea90
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2024-03-26 17:52:33 -0700

    [brief] Adds the functions to convert to onnx.
    [detailed]
    - Added the code to also convert using the new dynamo system. Still
      needs to be checked though.

---
+++ src/pyro/onnx.py
@@ -0,0 +1,86 @@
+import pathlib
+
+import numpy as np
+import onnx
+import onnxruntime  # type: ignore[import-untyped]
+import torch
+import torch.onnx
+from torch import nn
+
+
+def export_to_onnx(
+    net: nn.Module,
+    net_args: torch.Tensor,
+    out_path: pathlib.Path,
+    validate_output: bool = False,
+    rtol: float = 1e-3,
+    atol: float = 1e-5,
+    **kwargs,
+) -> None:
+    """
+    Export the given network to ONNX format.
+
+    By default, the resulting onnx network will be validated through ONNX to ensure it's
+    valid. If you wish to validate the traced outputs to ensure they're the same, set
+    validate_output to True and change rtol/atol as needed.
+
+    Args:
+        net (nn.Module): the network to convert.
+        net_args (torch.Tensor): the input tensor for tracing.
+        out_path (pathlib.Path): the path to save the exported network to.
+        validate_output (bool): if True, validation is performed to ensure correctness.
+        rtol (float): relative tolerance threshold.
+        atol (float): absolute tolerance threshold.
+        kwargs (dict): additional keyword arguments to torch.onnx.export.
+    """
+    net.eval()
+    with torch.no_grad():
+        out = net(net_args)
+
+    torch.onnx.export(net, net_args, out_path, **kwargs)
+
+    onnx_model = onnx.load(out_path)
+    onnx.checker.check_model(onnx_model)
+
+    if validate_output:
+        ort_session = onnxruntime.InferenceSession(
+            out_path, providers=["CPUExecutionProvider"]
+        )
+
+        def to_numpy(tensor: torch.Tensor):
+            if tensor.requires_grad:
+                return tensor.detach().cpu().numpy()
+            return tensor.cpu().numpy()
+
+        ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(net_args)}
+        ort_outs = ort_session.run(None, ort_inputs)
+        np.testing.assert_allclose(to_numpy(out), ort_outs[0], rtol=rtol, atol=atol)
+
+
+def export_to_onnx_dynamo(
+    out_path: pathlib.Path,
+    net: nn.Module,
+    *net_args,
+    export_options: torch.onnx.ExportOptions | None = None,
+    **net_kwargs,
+):
+    """
+    Export the given network to ONNX format using TorchDynamo.
+
+    The resulting onnx network will be checked for correctness after it is created, but no
+    further validation will be performed.
+
+    Args:
+        out_path (pathlib.Path): the path to save the exported network to.
+        net (nn.Module): the network to convert.
+        net_args: positional arguments for the network.
+        export_options (torch.onnx.ExportOptions | None): any export options.
+        net_kwargs: keyword arguments to the network.
+    """
+    onnx_program = torch.onnx.dynamo_export(
+        net, *net_args, **net_kwargs, export_options=export_options
+    )
+    onnx_program.save(out_path)
+
+    onnx_model = onnx.load(out_path)
+    onnx.checker.check_model(onnx_model)
