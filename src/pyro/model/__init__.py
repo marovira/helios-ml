b9e5bcd897bf311a566e2a2f2e561340022d5e52
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2024-04-04 16:11:27 -0700

    [brief] Adds unit tests for the model base class.
    [detailed]
    - Mostly just testing the default behaviours of the class.
    - Also adds the model registry to the init module.

--- src/pyro/model/__init__.py
+++ src/pyro/model/__init__.py
@@ -1,5 +1,5 @@
 """Module package for pyro."""
 
-from .model import Model, create_model, find_pretrained_file
+from .model import MODEL_REGISTRY, Model, create_model, find_pretrained_file
 
-__all__ = ["Model", "create_model", "find_pretrained_file"]
+__all__ = ["MODEL_REGISTRY", "Model", "create_model", "find_pretrained_file"]
--- test/test_model.py
+++ test/test_model.py
@@ -4,6 +4,8 @@ import typing
 import torch
 from torch import nn
 
+import pyro.model as pym
+import pyro.trainer as pyt
 from pyro import core
 from pyro.model import losses, metrics, optimizers, schedulers
 
@@ -149,3 +151,75 @@ class TestSchedulers:
 
     def test_create(self) -> None:
         check_create_function(schedulers.SCHEDULER_REGISTRY, schedulers.create_scheduler)
+
+
+@pym.MODEL_REGISTRY.register
+class MockModel(pym.Model):
+    def __init__(self) -> None:
+        super().__init__("mock-model")
+
+    @property
+    def loss_items(self) -> dict[str, torch.Tensor]:
+        return self._loss_items
+
+    @property
+    def val_scores(self) -> dict[str, float]:
+        return self._val_scores
+
+    @property
+    def test_scores(self) -> dict[str, float]:
+        return self._test_scores
+
+    def setup(self, fast_init: bool = False) -> None:
+        pass
+
+    def train_step(self, batch: typing.Any, state) -> None:
+        pass
+
+    def populate_loss(self) -> None:
+        self._loss_items["foo"] = torch.tensor(1)
+        self._loss_items["bar"] = torch.tensor(2)
+
+    def populate_val_scores(self) -> None:
+        self._val_scores["foo"] = 1
+        self._val_scores["bar"] = 2
+
+    def populate_test_scores(self) -> None:
+        self._test_scores["foo"] = 1
+        self._test_scores["bar"] = 2
+
+
+class TestModel:
+    def test_defaults(self) -> None:
+        model = MockModel()
+        state = pyt.TrainingState()
+
+        assert model.save_name == "mock-model"
+        assert model.state_dict() == {}
+
+        chkpt_name = "chkpt"
+        assert model.append_metadata_to_chkpt_name(chkpt_name) == chkpt_name
+
+        state_dict = {"a": 1, "b": 2}
+        assert model.strip_training_data(state_dict) == state_dict
+
+        model.populate_loss()
+        assert len(model.loss_items) != 0
+        model.on_training_batch_start(state)
+        assert len(model.loss_items) == 0
+
+        model.populate_val_scores()
+        assert len(model.val_scores) != 0
+        model.on_validation_start(0)
+        assert len(model.loss_items) == 0
+
+        assert model.have_metrics_improved()
+
+        model.populate_test_scores()
+        assert len(model.test_scores) != 0
+        model.on_testing_start()
+        assert len(model.test_scores) == 0
+
+    def test_create(self) -> None:
+        check_registry(pym.MODEL_REGISTRY, ["MockModel"])
+        pym.create_model("MockModel")
