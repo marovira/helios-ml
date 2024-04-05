841450a6cf0b26e68450e79c6cd1e5a53ed01a4f
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2024-04-05 15:54:14 -0700

    [brief] Adds a networks module.
    [detailed]
    - Apparently I just completely forgot to make a network registry.
    - Also adds the function to init weights since it seems useful.

--- test/test_model.py
+++ test/test_model.py
@@ -2,12 +2,34 @@ import dataclasses
 import typing
 
 import torch
+import torch.nn.functional as F
 from torch import nn
 
 import pyro.model as pym
 import pyro.trainer as pyt
 from pyro import core
-from pyro.model import losses, metrics, optimizers, schedulers
+from pyro.model import losses, metrics, networks, optimizers, schedulers
+
+
+@networks.NETWORK_REGISTRY.register
+class ClassifierNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 
 @losses.LOSS_REGISTRY.register
@@ -151,6 +173,14 @@ class TestSchedulers:
 
     def test_create(self) -> None:
         check_create_function(schedulers.SCHEDULER_REGISTRY, schedulers.create_scheduler)
+
+
+class TestNewtorks:
+    def test_registry(self) -> None:
+        check_registry(networks.NETWORK_REGISTRY, ["ClassifierNet"])
+
+    def test_create(self) -> None:
+        check_create_function(networks.NETWORK_REGISTRY, networks.create_network)
 
 
 @pym.MODEL_REGISTRY.register
