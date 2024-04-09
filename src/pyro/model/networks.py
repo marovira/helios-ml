0b4096f865f588023d54c6ace1fcccd0398cb791
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2024-04-08 17:04:20 -0700

    [brief] Adds the EMA wrapper.
    [detailed]
    - I thought we could just use the stuff from PyTorch but it seems... not
      particularly ergonomic. It feels like it was designed for relatively
      simple workflows, which Pyro is definitely not targetting.

--- src/pyro/model/networks.py
+++ src/pyro/model/networks.py
@@ -1,3 +1,4 @@
+import copy
 import typing
 
 import torch
@@ -54,3 +55,57 @@ def default_init_weights(
                 init.constant_(m.weight, 1)
                 if m.bias is not None:
                     m.bias.data.fill_(bias_fill)
+
+
+class EMA(nn.Module):
+    """
+    Implements Exponential Moving Average (EMA).
+
+    Args:
+        net (nn.Module): the bare network on which EMA will be performed.
+        decay (float): decay rate.
+        device (torch.device | None): the device to be used.
+    """
+
+    def __init__(
+        self, net: nn.Module, decay: float = 0.9997, device: torch.device | None = None
+    ):
+        """Create the EMA wrapper."""
+        super().__init__()
+
+        self._module = copy.deepcopy(net)
+        self._module = self._module.eval()
+        self._decay = decay
+        self._device = device
+
+        if self._device is not None:
+            self._module.to(device=device)
+
+    @torch.no_grad()
+    def _update(self, net: nn.Module, update_fn: typing.Callable) -> None:
+        for ema_v, net_v in zip(
+            self._module.state_dict().values(),
+            net.state_dict().values(),
+            strict=True,
+        ):
+            if self.device:
+                net_v = net_v.to(device=self.device)
+            ema_v.copy_(update_fn(ema_v, net_v))
+
+    def update(self, net: nn.Module) -> None:
+        """Update the weights using EMA from the given network."""
+        self._update(net, update_fn=lambda e, m: self.decay * e + (1.0 - self.decay) * m)
+
+    def set(self, net: nn.Module) -> None:
+        """Re-set the base weights."""
+        self._update(net, update_fn=lambda e, m: m)
+
+    def forward(self, *args: typing.Any, **kwargs: typing.Any):
+        """
+        Evaluate the EMA wrapper on the network inputs.
+
+        Args:
+            args (Any): named parameters for your network's forward function.
+            kwargs (Any): keyword arguments for your network's forward function.
+        """
+        return self.module(*args, **kwargs)
