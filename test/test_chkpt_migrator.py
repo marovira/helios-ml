e45a617c479bbb97bd66e17d5c001114b45b4098
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2024-07-18 13:59:39 -0700

    [brief] Adds a tool to migrate checkpoints between versions.
    [detailed]
    - This solves the problem of migrating checkpoints from pre-released
      versions of Helios to the first official release.
    - Also adds unit tests.

---
+++ test/test_chkpt_migrator.py
@@ -0,0 +1,39 @@
+import pathlib
+
+import torch
+
+import helios
+import helios.trainer as hlt
+from helios.chkpt_migrator import migrate_checkpoints_to_current_version
+
+
+class TestChkptMigrator:
+    def test_from_pre_release(self, tmp_path: pathlib.Path) -> None:
+        state = {
+            "training_state": hlt.TrainingState(),
+            "model": {},
+            "rng": {},
+        }
+
+        torch.save(state, tmp_path / "state.pth")
+        migrate_checkpoints_to_current_version(tmp_path)
+
+        new_state = torch.load(tmp_path / "state.pth")
+        assert all(
+            key in new_state for key in ("version", "training_state", "model", "rng")
+        )
+        assert new_state["version"] == helios.__version__
+
+    def test_from_current_release(self, tmp_path: pathlib.Path) -> None:
+        state = {
+            "training_state": hlt.TrainingState(),
+            "model": {},
+            "rng": {},
+            "version": helios.__version__,
+        }
+
+        torch.save(state, tmp_path / "state.pth")
+        migrate_checkpoints_to_current_version(tmp_path)
+
+        new_state = torch.load(tmp_path / "state.pth")
+        assert state == new_state
