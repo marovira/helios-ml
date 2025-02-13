587b2bf8510662fb588b4bf109b0d2457672aac7
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2025-02-12 16:55:00 -0800

    [brief] Adds a way to avoid saving an exported ONNX network if validation fails.
    [detailed]

--- src/helios/onnx.py
+++ src/helios/onnx.py
@@ -13,6 +13,7 @@ def export_to_onnx(
     net_args: torch.Tensor,
     out_path: pathlib.Path,
     validate_output: bool = False,
+    save_on_validation_fail: bool = True,
     rtol: float = 1e-3,
     atol: float = 1e-5,
     **kwargs,
@@ -24,12 +25,18 @@ def export_to_onnx(
     valid. If you wish to validate the traced outputs to ensure they're the same, set
     ``validate_output`` to true and change ``rtol``/``atol`` as needed.
 
+    .. warning::
+        This function assumes that the given network takes a single tensor as input and
+        produces a single tensor as output.
+
     Args:
         net: the network to convert.
         net_args: the input tensor for tracing.
         out_path: the path to save the exported network to.
         validate_output: if true, validation is performed to ensure correctness. Defaults
             to false.
+        save_on_validation_fail: if true, the ONNX network is saved regardless of whether
+            validation succeeds. If false, the ONNX network is deleted. Defaults to true.
         rtol: relative tolerance threshold. Defaults to ``1e-3``.
         atol: absolute tolerance threshold. Defaults to ``1e-5``.
         kwargs: additional keyword arguments to ``torch.onnx.export``.
@@ -38,7 +45,7 @@ def export_to_onnx(
     with torch.no_grad():
         out = net(net_args)
 
-    torch.onnx.export(net, net_args, str(out_path), **kwargs)  # type: ignore[arg-type]
+    torch.onnx.export(net, net_args, out_path, **kwargs)  # type: ignore[arg-type]
 
     onnx.checker.check_model(out_path)
 
@@ -54,4 +61,9 @@ def export_to_onnx(
 
         ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(net_args)}
         ort_outs = ort_session.run(None, ort_inputs)
-        np.testing.assert_allclose(to_numpy(out), ort_outs[0], rtol=rtol, atol=atol)
+        try:
+            np.testing.assert_allclose(to_numpy(out), ort_outs[0], rtol=rtol, atol=atol)
+        except AssertionError as e:
+            if not save_on_validation_fail:
+                out_path.unlink(missing_ok=True)
+            raise e
