5d09f8bd90e5b5f855b99fbb2273fee22469b8a3
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2026-04-03 22:54:47 -0700

    Re-export common classes at the package level
    This is mostly for usability purposes and to make life easier when using
    Helios.

--- src/helios/__init__.py
+++ src/helios/__init__.py
@@ -1,5 +1,21 @@
 """A light-weight package for training AI models."""
 
 from ._version import __version__
+from .data import DataLoaderParams, DataModule, DatasetSplit
+from .model import AMPState, Model
+from .plugins import Plugin
+from .trainer import Trainer, TrainingState, TrainingUnit, find_last_checkpoint
 
-__all__ = ["__version__"]
+__all__ = [
+    "__version__",
+    "AMPState",
+    "DataLoaderParams",
+    "DataModule",
+    "DatasetSplit",
+    "find_last_checkpoint",
+    "Model",
+    "Plugin",
+    "Trainer",
+    "TrainingState",
+    "TrainingUnit",
+]
